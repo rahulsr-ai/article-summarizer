2615322d1445846cc0377d77745b2b756a9b3685
eternal_student <student1742003@gmail.com>
2025-05-17 17:12:11 +0530

    first commit

--- src/services/Article.js
+++ src/services/Article.js
@@ -1,30 +1,3 @@
-// import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
-
-// const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
-
-// export const articleApi = createApi({
-//   reducerPath: "articleApi",
-//   baseQuery: fetchBaseQuery({
-//     baseUrl: "https://article-extractor-and-summarizer.p.rapidapi.com/",
-//     prepareHeaders: (headers) => {
-//       headers.set("x-rapidapi-key", rapidApiKey);
-//       headers.set(
-//         "x-rapidapi-host",
-//         "article-extractor-and-summarizer.p.rapidapi.com"
-//       );
-
-//       return headers;
-//     },
-//   }),
-//   endpoints: (build) => ({
-//     getSummary: build.query({
-//       query: (params) => `/summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`,
-//     }),
-//   }),
-// });
-
-// export const { GetSummaryQuery } = articleApi;
-
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"; // Redux Toolkit ka function jo API integration simplify karta hai
 
 const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY; // API key environment variable se le rahe hain for security
